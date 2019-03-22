424696b676db080598a4ad985f99d50aa279b5a8
Lichen Ma <lichenma123@gmail.com>
2019-03-22 13:24:35 -0400

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,19 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'All Might' },
+  { id: 12, name: 'Bakugo' },
+  { id: 13, name: 'Midoriya' },
+  { id: 14, name: 'Uraraka' },
+  { id: 15, name: 'Iida' },
+  { id: 16, name: 'Todoroki' },
+  { id: 17, name: 'Yaoyorozu' },
+  { id: 18, name: 'Saitama' },
+  { id: 19, name: 'Genos' },
+  { id: 20, name: 'SilverFang' },
+  { id: 21, name: 'King' },
+  { id: 22, name: 'Tatsumaki' },
+  { id: 23, name: 'MumenRider' },
+  { id: 24, name: 'Fubuki' },
+  { id: 25, name: 'Atomic Samurai' }
+];
